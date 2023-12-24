304752ff393128af2673c803428c5b44db91009a
Michael Feleke <felekemichael999@gmail.com>
2023-12-24 19:20:44 +0300

    Building the App Layout

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import PageNotFound from "./pages/PageNotFound";
 import Users from "./pages/Users";
 
 import GlobalStyles from "./styles/GlobalStyles";
+import AppLayout from "./ui/Applayout";
 
 function App() {
   return (
@@ -16,13 +17,15 @@ function App() {
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
-          <Route index element={<Navigate replace to="dashboard" />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="bookings" element={<Bookings />} />
-          <Route path="cabins" element={<Cabins />} />
-          <Route path="users" element={<Users />} />
-          <Route path="settings" element={<Settings />} />
-          <Route path="account" element={<Account />} />
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="users" element={<Users />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="account" element={<Account />} />
+          </Route>
           <Route path="login" element={<Login />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
