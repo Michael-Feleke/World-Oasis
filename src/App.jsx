b6f9453eaeef60500cdce37c0b38a1ba1ec6a4f1
Michael Feleke <felekemichael999@gmail.com>
2023-12-24 18:28:06 +0300

    Building a reusable button with styled component

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
 import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
 const StyledApp = styled.div`
-  background-color: cyan;
   padding: 20px;
 `;
 
@@ -14,13 +14,25 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <Heading as="h1">The Wild Oasis</Heading>
-        <Heading as="h2">Check in /out</Heading>
-        <Button>Check in</Button>
-        <Button>Check out</Button>
-        <Input type="number" placeholder="Number of guests" />
-        <Input type="number" placeholder="Number of guests" />
-        <Heading as="h3">Form</Heading>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">The Wild Oasis</Heading>
+            <div>
+              <Heading as="h2">Check in /out</Heading>
+              <Button>Check in</Button>
+              <Button variation="secondary" size="small">
+                Check out
+              </Button>
+            </div>
+          </Row>
+          <Row>
+            <Heading as="h3">Form</Heading>
+            <form>
+              <Input type="number" placeholder="Number of guests" />
+              <Input type="number" placeholder="Number of guests" />
+            </form>
+          </Row>
+        </Row>
       </StyledApp>
     </>
   );
--- src/ui/Heading.jsx
+++ src/ui/Heading.jsx
@@ -6,24 +6,20 @@ const Heading = styled.h1`
     css`
       font-size: 3rem;
       font-weight: 600;
-      background-color: yellow;
     `}
   ${(props) =>
     props.type == "h2" &&
     css`
       font-size: 2rem;
       font-weight: 600;
-      background-color: yellow;
     `}
   ${(props) =>
     props.type == "h3" &&
     css`
       font-size: 2rem;
       font-weight: 500;
-      background-color: yellow;
     `}
 
   line-height:1.4;
 `;
-
 export default Heading;
