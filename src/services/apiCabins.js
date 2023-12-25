0be2dcdb3aaa616987adb3bf9b3c8ea0ee86728c
Michael Feleke <felekemichael999@gmail.com>
2023-12-25 18:42:28 +0300

    Mutations to delete a cabin

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -10,3 +10,13 @@ export async function getCabins() {
 
   return data;
 }
+
+export async function deleteCabin(id) {
+  const { data, error } = await supabase.from("cabins").delete().eq("id", id);
+  if (error) {
+    console.error(error);
+    throw new Error(`Cabins couldn't be deleted`);
+  }
+
+  return data;
+}
