8622097502a899a0aeb4badfc27b2dfce1412319
Michael Feleke <felekemichael999@gmail.com>
2023-12-26 16:22:35 +0300

    Updating Application Settings

---
+++ src/features/settings/useUpdateSetting.js
@@ -0,0 +1,19 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+import { updateSetting as updateSettingApi } from "../../services/apiSettings";
+
+function useUpdateSetting() {
+  const queryClient = useQueryClient();
+  const { mutate: updateSetting, isLoading: isUpdating } = useMutation({
+    mutationFn: updateSettingApi,
+    onSuccess: () => {
+      toast.success("Setting edited successfully!");
+      queryClient.invalidateQueries({ queryKey: ["settings"] });
+    },
+    onError: (err) => toast.error(err.message),
+  });
+
+  return { isUpdating, updateSetting };
+}
+
+export { useUpdateSetting };
