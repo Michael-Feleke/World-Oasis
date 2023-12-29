9a140a2a69dd8c3255fcb19ac1197877843d0142
Michael Feleke <felekemichael999@gmail.com>
2023-12-29 10:44:14 +0300

    fix:making the Booking api flexible

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -8,7 +8,8 @@ export async function getBookings({ filter, sortBy }) {
       "id,created_at,startDate,endDate,numNights,numGuests,status,totalPrice,cabins(name),guests(fullName,email)"
     );
 
-  if (filter !== null) query = query.eq(filter.field, filter.value);
+  if (filter !== null)
+    query = query[filter.method || "eq"](filter.field, filter.value);
 
   const { data, error } = await query;
 
