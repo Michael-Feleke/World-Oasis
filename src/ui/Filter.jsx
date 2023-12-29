659f4550488d2c5cabe08645187c8c4e5d74d2ca
Michael Feleke <felekemichael999@gmail.com>
2023-12-29 09:24:27 +0300

    feat-implement sorting cabin

--- src/ui/Filter.jsx
+++ src/ui/Filter.jsx
@@ -50,6 +50,7 @@ function Filter({ filterField, options }) {
         <FilterButton
           onClick={() => handleClick(option.value)}
           active={option.value === currentFilter}
+          disabled={option.value === currentFilter}
           key={option.value}
         >
           {option.label}
