78b57370ec282662204266b216cb9928d82fc0e1
Michael Feleke <felekemichael999@gmail.com>
2023-12-30 15:17:59 +0300

    feat:building the app header

---
+++ src/ui/HeaderMenu.jsx
@@ -0,0 +1,29 @@
+import ButtonIcon from "./ButtonIcon";
+import { HiOutlineUser } from "react-icons/hi2";
+import Logout from "../features/authentication/Logout";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+const StyledHeaderMenu = styled.ul`
+  display: flex;
+  gap: 0.4rem;
+`;
+
+function HeaderMenu() {
+  const navigate = useNavigate();
+
+  return (
+    <StyledHeaderMenu>
+      <li>
+        <ButtonIcon onClick={() => navigate("/account")}>
+          <HiOutlineUser />
+        </ButtonIcon>
+      </li>
+      <li>
+        <Logout />
+      </li>
+    </StyledHeaderMenu>
+  );
+}
+
+export default HeaderMenu;
