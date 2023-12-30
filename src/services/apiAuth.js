994e1d7059c997a26a1573b54b294a900a16e635
Michael Feleke <felekemichael999@gmail.com>
2023-12-30 15:53:34 +0300

    feat: implementing updating user avatar

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,4 +1,4 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
 export async function signup({ fullName, email, password }) {
   const { data, error } = await supabase.auth.signUp({
@@ -53,4 +53,18 @@ export async function updateCurrentUser({ password, fullName, avatar }) {
 
   if (error) throw new Error(error.message);
   if (!avatar) data;
+
+  const fileName = `avatar-${data.user.id}-${Math.random()}`;
+  const { error: uploadAvatarError } = await supabase.storage
+    .from("avatars")
+    .upload(fileName, avatar);
+  if (uploadAvatarError) throw new Error(uploadAvatarError.message);
+
+  const { data: updatedUser, error: error2 } = supabase.auth.updateUser({
+    data: {
+      avatar: `${supabaseUrl}/storage/v1/object/public/avatars/${fileName}`,
+    },
+  });
+  if (error2) throw new Error(error2.message);
+  return updatedUser;
 }
