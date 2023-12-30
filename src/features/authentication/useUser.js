3fcc6190a0dbb6453b7d73a954ac4724a00f686e
Michael Feleke <felekemichael999@gmail.com>
2023-12-30 10:36:02 +0300

    feat: create useUser to get user info from supabase

---
+++ src/features/authentication/useUser.js
@@ -0,0 +1,13 @@
+import { useQuery } from "@tanstack/react-query";
+import { getCurrentUser } from "../../services/apiAuth";
+
+function useUser() {
+  const { isLoading, data: user } = useQuery({
+    queryFn: getCurrentUser,
+    queryKey: ["user"],
+  });
+
+  return { isLoading, user };
+}
+
+export { useUser };
