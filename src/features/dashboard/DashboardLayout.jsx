640af6db39504236348926c4b905183eace7dffd
Michael Feleke <felekemichael999@gmail.com>
2023-12-30 19:39:21 +0300

    feat: building the dashboard layout

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -6,3 +6,15 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  return (
+    <StyledDashboardLayout>
+      <div>stat</div>
+      <div>Act</div>
+      <div>Chart</div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
